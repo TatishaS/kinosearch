6d9b88162be4a89363291147d277314427e77424
Tatiana <snow_white_girl@bk.ru>
2021-07-09 13:54:47 +0300

    Refactored functions order in the script

--- script-fetch.js
+++ script-fetch.js
@@ -9,14 +9,112 @@ const API_URL = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&la
 const API_TRENDING = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=ru-RU`;
 const POSTER_URL = 'https://image.tmdb.org/t/p/w500/';
 
-function addMediaHandler() {
-  const media = movie.querySelectorAll('img[data-id]');
-  media.forEach(function (elem) {
-    elem.style.cursor = 'pointer';
-    elem.addEventListener('click', showFullInfo);
-  });
+/* GETTING TRENDING MOVIES */
+document.addEventListener('DOMContentLoaded', function () {
+  fetch(API_TRENDING)
+    .then(function (value) {
+      if (value.status !== 200) {
+        return Promise.reject(new Error(value.status));
+      }
+      return value.json();
+    })
+    .then(function (output) {
+      let html =
+        '<h4 class="col-12 text-center text-info">Популярные за неделю</h4>';
+      if (output.results.length === 0) {
+        html =
+          '<h2 class="col-12 text-center text-info">Ничего не найдено</h2>';
+      }
+      output.results.forEach(function (item) {
+        let movieName = item.name || item.title;
+        let mediaType = item.title ? 'movie' : 'tv';
+        let movieReleaseDate = new Date(
+          item.release_date || item.first_air_date
+        ).toLocaleString('ru', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        });
+
+        const poster = item.poster_path
+          ? POSTER_URL + item.poster_path
+          : './img/no-poster.jpg';
+
+        let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
+
+        html += `
+    <div class="col-12 col-md-6 col-xl-3 item">
+    <img src=${poster} alt="${movieName}" ${dataInfo}>
+    <h5>${movieName}(${movieReleaseDate})</h5>
+    </div>`;
+      });
+      movie.innerHTML = html;
+      addMediaHandler();
+    })
+    .catch(err => {
+      movie.innerHTML = `Упс! Что-то пошло не так. Ошибка ${err.status}`;
+      console.log(err.status);
+    });
+});
+
+/* GETTING SEARCH RESULTS */
+function apiSearch(e) {
+  e.preventDefault();
+  const searchText = document.querySelector('.form-control').value;
+  if (searchText.trim().length === 0) {
+    movie.innerHTML =
+      '<h2 class="col-12 text-center text-danger">Поле поиска должно быть заполнено!</h2>';
+    return;
+  }
+  const server = API_URL + searchText;
+  movie.innerHTML = '<div class="spinner"></div>';
+
+  fetch(server)
+    .then(function (value) {
+      if (value.status !== 200) {
+        return Promise.reject(new Error(value.status));
+      }
+      return value.json();
+    })
+    .then(function (output) {
+      let html = '';
+      if (output.results.length === 0) {
+        html =
+          '<h2 class="col-12 text-center text-info">Фильмы с таким названием не найдены</h2>';
+      }
+      output.results.forEach(function (item, i) {
+        let movieName = item.name || item.title;
+        let movieReleaseDate = new Date(
+          item.release_date || item.first_air_date
+        ).toLocaleString('ru', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        });
+
+        const poster = item.poster_path
+          ? POSTER_URL + item.poster_path
+          : './img/no-poster.jpg';
+
+        let dataInfo = '';
+        if (item.media_type === 'movie' || item.media_type === 'tv')
+          dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+        html += `
+        <div class="col-12 col-md-6 col-xl-3 item">
+        <img src=${poster} alt="${movieName}" ${dataInfo}>
+        <h5>${movieName}(${movieReleaseDate})</h5>
+        </div>`;
+      });
+      movie.innerHTML = html;
+      addMediaHandler();
+    })
+    .catch(err => {
+      movie.innerHTML = `Упс! Что-то пошло не так. Ошибка ${err.status}`;
+      console.log(err || err.status);
+    });
 }
 
+/* GETTING MOVIE DESCRIPTION */
 function showFullInfo() {
   let url = '';
   const typeMedia = this.dataset.type;
@@ -82,111 +180,7 @@ function showFullInfo() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  fetch(API_TRENDING)
-    .then(function (value) {
-      if (value.status !== 200) {
-        return Promise.reject(new Error(value.status));
-      }
-      return value.json();
-    })
-    .then(function (output) {
-      let html =
-        '<h4 class="col-12 text-center text-info">Популярные за неделю</h4>';
-      if (output.results.length === 0) {
-        html =
-          '<h2 class="col-12 text-center text-info">Ничего не найдено</h2>';
-      }
-      output.results.forEach(function (item) {
-        let movieName = item.name || item.title;
-        let mediaType = item.title ? 'movie' : 'tv';
-        let movieReleaseDate = new Date(
-          item.release_date || item.first_air_date
-        ).toLocaleString('ru', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        });
-
-        const poster = item.poster_path
-          ? POSTER_URL + item.poster_path
-          : './img/no-poster.jpg';
-
-        let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
-
-        html += `
-    <div class="col-12 col-md-6 col-xl-3 item">
-    <img src=${poster} alt="${movieName}" ${dataInfo}>
-    <h5>${movieName}(${movieReleaseDate})</h5>
-    </div>`;
-      });
-      movie.innerHTML = html;
-      addMediaHandler();
-    })
-    .catch(err => {
-      movie.innerHTML = `Упс! Что-то пошло не так. Ошибка ${err.status}`;
-      console.log(err.status);
-    });
-});
-
-function apiSearch(e) {
-  e.preventDefault();
-  const searchText = document.querySelector('.form-control').value;
-  if (searchText.trim().length === 0) {
-    movie.innerHTML =
-      '<h2 class="col-12 text-center text-danger">Поле поиска должно быть заполнено!</h2>';
-    return;
-  }
-  const server = API_URL + searchText;
-  movie.innerHTML = '<div class="spinner"></div>';
-
-  fetch(server)
-    .then(function (value) {
-      if (value.status !== 200) {
-        return Promise.reject(new Error(value.status));
-      }
-      return value.json();
-    })
-    .then(function (output) {
-      let html = '';
-      if (output.results.length === 0) {
-        html =
-          '<h2 class="col-12 text-center text-info">Фильмы с таким названием не найдены</h2>';
-      }
-      output.results.forEach(function (item, i) {
-        let movieName = item.name || item.title;
-        let movieReleaseDate = new Date(
-          item.release_date || item.first_air_date
-        ).toLocaleString('ru', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        });
-
-        const poster = item.poster_path
-          ? POSTER_URL + item.poster_path
-          : './img/no-poster.jpg';
-
-        let dataInfo = '';
-        if (item.media_type === 'movie' || item.media_type === 'tv')
-          dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
-        html += `
-        <div class="col-12 col-md-6 col-xl-3 item">
-        <img src=${poster} alt="${movieName}" ${dataInfo}>
-        <h5>${movieName}(${movieReleaseDate})</h5>
-        </div>`;
-      });
-      movie.innerHTML = html;
-      addMediaHandler();
-    })
-    .catch(err => {
-      movie.innerHTML = `Упс! Что-то пошло не так. Ошибка ${err.status}`;
-      console.log(err || err.status);
-    });
-}
-
-searchForm.addEventListener('submit', apiSearch);
-
+/* GETTING VIDEO TRAILER */
 function getVideo(type, id) {
   let youtube = movie.querySelector('.youtube');
   youtube.innerHTML = type;
@@ -202,7 +196,8 @@ function getVideo(type, id) {
     .then(output => {
       let videoFrame = '<h4 class="col-12 text-info">Трейлеры</h4>';
       if (output.results.length < 1) {
-        videoFrame = '<p>Доступные для просмотра трейлеры отсутствуют</p>';
+        videoFrame =
+          '<p class="text-warning">Доступные для просмотра трейлеры отсутствуют</p>';
       }
 
       output.results.forEach(item => {
@@ -216,4 +211,15 @@ function getVideo(type, id) {
       youtube.innerHTML = `Видео отсутствует`;
       console.log(err.status);
     });
+}
+
+/* HANDLERS */
+searchForm.addEventListener('submit', apiSearch);
+
+function addMediaHandler() {
+  const media = movie.querySelectorAll('img[data-id]');
+  media.forEach(function (elem) {
+    elem.style.cursor = 'pointer';
+    elem.addEventListener('click', showFullInfo);
+  });
 }
